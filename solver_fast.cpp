9615713f3dd2ac121afd3e176d671cb3ba70df38
idaten <shukai2013@gmail.com>
2020-02-27 19:20:30 +0900

    実装：ジェネレーター

--- solver_fast.cpp
+++ solver_fast.cpp
@@ -1,158 +1,117 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-using board = array<array<int, 9>, 9>;
+using board = array<array<int,9>,9>;
 
-class solver_fast
-{
+class solver_fast{
 public:
     board b;
     const int sz = 9;
     const int rsz = 3;
     int cnt = 0;
-    solver_fast() {}
-
-    void input()
-    {
-        for (int i = 0; i < sz; ++i)
-        {
+    solver_fast(){}
+    
+    void input() {
+        for (int i = 0; i < sz; ++i) {
             string s;
             cin >> s;
-            for (int j = 0; j < sz; ++j)
-            {
-                int num = s[j] - '0';
-                if (1 <= num && num <= sz)
-                {
+            for (int j = 0; j < sz; ++j) {
+                int num = s[j]-'0';
+                if (1<= num && num <= sz) {
                     b[i][j] = num;
-                }
-                else
-                {
+                }else{
                     b[i][j] = 0;
                 }
             }
         }
     }
-    int get(int y, int x)
-    {
+    int get(int y, int x) {
         return b[y][x];
     }
-    void set(int y, int x, int num)
-    {
+    void set(int y,int x, int num){
         b[y][x] = num;
     }
-    void print()
-    {
-        for (int i = 0; i < sz; ++i)
-        {
-            for (int j = 0; j < sz; ++j)
-            {
-                cout << get(i, j) << " ";
+    void print(){
+        for(int i=0;i<sz;++i){
+            for(int j=0;j<sz;++j){
+                cout << get(i,j) << " ";
             }
             cout << "\n";
         }
     }
     // 変更が(y,x)のみであることが保証されている
-    bool check(int y, int x)
-    {
-        if (!check_horizon(y, x))
-            return false;
-        if (!check_vertical(y, x))
-            return false;
-        if (!check_square(y, x))
-            return false;
+    bool check(int y,int x){
+        if(!check_horizon(y, x))return false;
+        if(!check_vertical(y, x))return false;
+        if(!check_square(y, x))return false;
         return true;
     }
-    bool check_horizon(int y, int x)
-    {
-        int num = get(y, x);
+    bool check_horizon(int y,int x){
+        int num = get(y,x);
         int tmp = 0;
-        for (int i = 0; i < sz; ++i)
-        {
-            if (get(i, x) == num)
-                tmp++;
-            if (tmp > 1)
-                return false;
+        for(int i=0;i<sz;++i){
+            if(get(i,x)==num)tmp++;
+            if(tmp>1)return false;
         }
         return true;
     }
-    bool check_vertical(int y, int x)
-    {
-        int num = get(y, x);
+    bool check_vertical(int y,int x){
+        int num = get(y,x);
         int tmp = 0;
-        for (int j = 0; j < sz; ++j)
-        {
-            if (get(y, j) == num)
-                tmp++;
-            if (tmp > 1)
-                return false;
+        for(int j=0;j<sz;++j){
+            if(get(y,j)==num)tmp++;
+            if(tmp>1)return false;
         }
         return true;
     }
-    bool check_square(int y, int x)
-    {
-        int num = get(y, x);
+    bool check_square(int y,int x){
+        int num = get(y,x);
         int tmp = 0;
-        int sy = y / rsz, sx = x / rsz;
-        for (int i = sy * rsz; i < (sy + 1) * rsz; ++i)
-        {
-            for (int j = sx * rsz; j < (sx + 1) * rsz; ++j)
-            {
-                if (get(i, j) == num)
-                    tmp++;
-                if (tmp > 1)
-                    return false;
+        int sy = y/rsz,sx = x/rsz;
+        for(int i = sy*rsz;i<(sy+1)*rsz;++i){
+            for(int j = sx*rsz;j<(sx+1)*rsz;++j){
+                if(get(i,j)==num)tmp++;
+                if(tmp>1)return false;
             }
         }
         return true;
     }
     board sol;
-    void solve()
-    {
-        for (int i = 0; i < sz; ++i)
-        {
-            for (int j = 0; j < sz; ++j)
-            {
+    void solve(){
+        for(int i=0;i<sz;++i){
+            for(int j=0;j<sz;++j){
                 sol[i][j] = b[i][j];
             }
         }
-        dfs(0, 0);
+        dfs(0,0);
         return;
     }
-    void dfs(int y, int x)
-    {
-        if (y == sz && x == 0)
-        {
+    void dfs(int y,int x){
+        if(y==sz&&x==0){
             cnt++;
             print();
             return;
         }
-        int ny = y, nx = x + 1;
-        if (nx == sz)
-        {
+        int ny = y, nx = x+1;
+        if(nx==sz){
             nx = 0;
             ny++;
         }
-        if (get(y, x) > 0)
-        {
-            dfs(ny, nx);
-        }
-        else
-        {
-            for (int i = 1; i <= sz; ++i)
-            {
-                set(y, x, i);
-                if (check(y, x))
-                {
-                    dfs(ny, nx);
+        if(get(y,x)>0){
+            dfs(ny,nx);
+        }else{
+            for(int i=1;i<=sz;++i){
+                set(y,x,i);
+                if(check(y,x)){
+                    dfs(ny,nx);
                 }
-                set(y, x, 0);
+                set(y,x,0);
             }
         }
     }
 };
 
-int main()
-{
+int main() {
     solver_fast sf;
     sf.input();
     auto start = clock();
